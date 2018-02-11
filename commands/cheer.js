33c2fbe0f895ef6a9f0607b0a830f9c1ab61c1a0
runepolaris <36331851+runepolaris@users.noreply.github.com>
2018-02-11 19:39:35 +0800

    Update cheer.js

--- commands/cheer.js
+++ commands/cheer.js
@@ -1,6 +1,6 @@
 module.exports = {
   exec: (client, message, content, args) => {
-    if (args) message.send("Ganbarou, " + args + "-[s]! You can do it!", "smile")
-    else message.send("Ganbarou senpai! You can do it!", "smile");
+    if (args) message.send("Let's do this, " + args + "-[s]! You can do it!", "smile")
+    else message.send("Good luck, Captain! You can do it!", "smile");
   }
 }
